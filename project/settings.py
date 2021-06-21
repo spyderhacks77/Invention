25accd8c933d45a6848f2c54d286a2039018afc2
spyderhacks77 <82039891+spyderhacks77@users.noreply.github.com>
2021-06-21 20:36:18 +0530

    Commit

--- project/settings.py
+++ project/settings.py
@@ -124,8 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
