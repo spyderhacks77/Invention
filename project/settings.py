a753306e709846a7cc212a1d3bdf233926ad3e85
spyderhacks77 <82039891+spyderhacks77@users.noreply.github.com>
2021-06-21 20:01:50 +0530

    Commit

--- project/settings.py
+++ project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(prod_db)
+DATABASES=['default'].update(prod_db)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
